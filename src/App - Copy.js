9d799d8b06f9429039f4f346978d2cc52ec7c435
TheresaHHH <hsuanhuahung@ufl.edu>
2023-11-02 00:37:21 -0400

    first commit

---
+++ src/App - Copy.js
@@ -0,0 +1,14 @@
+// src/App.js
+import React from 'react';
+import './App.css';
+import LanguagePreference from './LanguagePreference';
+
+function App() {
+  return (
+    <div className="App">
+      <LanguagePreference />
+    </div>
+  );
+}
+
+export default App;
--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [fontSize, setFontSize] = useState('medium');
+  const [fontColor, setFontColor] = useState('black');
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [language, setLanguage] = useState('english');
+
+  const handleLanguageChange = (newLanguage) => {
+    setLanguage(newLanguage);
+  };
+
+  const handleApplyChanges = () => {
+    setIsModalOpen(false);
+  };
+
+  const getFontSizeInPixels = (size) => {
+    switch (size) {
+      case 'small':
+        return '10px';
+      case 'medium':
+        return '14px';
+      case 'large':
+        return '24px';
+      default:
+        return '14px'; // Default to medium if size is invalid
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => setIsModalOpen(true)}>Customize Text</button>
+      {isModalOpen && (
+        <div className="modal">
+          <div className="modal-content">
+            <span className="close" onClick={() => setIsModalOpen(false)}>
+              &times;
+            </span>
+            <h2>Text Customization</h2>
+            <label htmlFor="fontSize">Font Size:</label>
+            <select
+              id="fontSize"
+              value={fontSize}
+              onChange={(e) => setFontSize(e.target.value)}
+            >
+              <option value="small">Small (10px)</option>
+              <option value="medium">Medium (14px)</option>
+              <option value="large">Large (24px)</option>
+            </select>
+            <br />
+            <label htmlFor="fontColor">Font Color:</label>
+            <input
+              type="color"
+              id="fontColor"
+              value={fontColor}
+              onChange={(e) => setFontColor(e.target.value)}
+            />
+            <br />
+            <button onClick={handleApplyChanges}>Apply Changes</button>
+
+            <button onClick={() => handleLanguageChange('english')}>English</button>
+            <button onClick={() => handleLanguageChange('spanish')}>Spanish</button>
+          </div>
+        </div>
+      )}
+      <p style={{ fontSize: getFontSizeInPixels(fontSize), color: fontColor }}>
+        {language === 'english' ? 'Hello, World!' : '¡Hola, Mundo!'}
+      </p>
     </div>
   );
 }
